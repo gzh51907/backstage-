984c2ef862af1f00f59807b49235e2ca28cd7bf0
guowei4396 <326411812@qq.com>
2019-11-04 22:18:23 +0800

    add goods

--- src/static/subassembly/merchandise/addlist.js
+++ src/static/subassembly/merchandise/addlist.js
@@ -1,119 +1,121 @@
-import React,{Component} from "react";
+import React, { Component } from "react";
 import Api from "APi";
-import { Layout, Menu, Icon,List, message,Button} from 'antd';
+import { Layout, Menu, Icon, List, message, Button } from 'antd';
 const ButtonGroup = Button.Group;
 import './addlist.css'
-class Addlist  extends Component{
-        state = {
-          datas: [],
-          loading: false,
-          hasMore: true,
-          timer:null,
-          page:1
-        };
+class Addlist extends Component {
+  state = {
+    datas: [],
+    loading: false,
+    hasMore: true,
+    timer: null,
+    page: 1
+  };
   // 对应id删除
-   renmoid = async (id) =>{
-      console.log("123" ,id );
-        let  {data}= await Api.deletel({
-              goodsId:id
-         })
+  renmoid = async (id) => {
+    // console.log("123", id);
+    let data = await Api.deletel({
+      goodsId: id
+    })
+    console.log(data);
+    let newdata = this.state.datas.filter(item => item.goodsId != id);
+    this.setState({
+      datas: newdata
+    })
+  }
 
-         console.log(data);
-         
+
+  handleScroll = () => {
+    // console.log(this.refs.tygs.childNodes[this.refs.tygs.childNodes.length - 1]);
+    var scrollTop =
+      window.pageYOffset ||
+      document.documentElement.scrollTop ||
+      document.body.scrollTop;
+    var offsetTop = this.refs.find.childNodes[this.refs.find.childNodes.length - 1].offsetTop;
+    // console.log(scrollTop + 650, offsetTop);
+    if (scrollTop + 650 >= offsetTop) {
+      clearTimeout(this.state.timer);
+      this.setState({
+        timer: setTimeout(async () => {
+          let page = this.state.page;
+          this.setState({
+            page: page + 1
+          });
+          let { data: newdata } = await Api.getlist({
+            num: 9,
+            page: this.state.page
+
+          });
+          let olddata = this.state.datas;
+          newdata.forEach(item => {
+            olddata.push(item);
+          })
+          this.setState({
+            datas: olddata
+          });
+          console.log(this.state.datas);
+        }, 1000)
+      });
     }
-
-
-      handleScroll = () => {
-        // console.log(this.refs.tygs.childNodes[this.refs.tygs.childNodes.length - 1]);
-        var scrollTop =
-            window.pageYOffset ||
-            document.documentElement.scrollTop ||
-            document.body.scrollTop;
-        var offsetTop = this.refs.find.childNodes[this.refs.find.childNodes.length - 1].offsetTop;
-        // console.log(scrollTop + 620, offsetTop);
-        if (scrollTop + 620 >= offsetTop) {
-            clearTimeout(this.state.timer);
-            this.setState({
-                timer: setTimeout(async () => {
-                    let page = this.state.page;
-                    this.setState({
-                        page: page + 1
-                    });
-                    let { data: newdata }=await Api.getlist({
-                      num:9,
-                      page:this.state.page
-                   
-                 });
-                    let olddata = this.state.datas;
-                    newdata.forEach(item => {
-                        olddata.push(item);
-                    })
-                    this.setState({
-                        datas: olddata
-                    });
-                    console.log(this.state.datas);
-                }, 1000)
-            });
-        }
-    }
-   async componentDidMount(){
+  }
+  async componentDidMount() {
 
     window.addEventListener("scroll", this.handleScroll);
-        let {data}=await Api.getlist({
-             num:9,
-             page:1
-          
-        })
-       this.setState({
-             datas:data
-         })
+    let { data } = await Api.getlist({
+      num: 9,
+      page: 1
 
-         
-   }
+    })
+    this.setState({
+      datas: data
+    })
 
-   errorPic = (tygimg) => {
+
+  }
+
+  errorPic = (tygimg) => {
     this.refs[tygimg].src = "../../../action/img/Clelo.jpg";
-}
+  }
 
 
 
   // 在生命周期函数销毁
   componentWillUnmount() {
     window.removeEventListener("scroll", this.handleScroll);
-}
+  }
 
-render(){
-    let {datas}=this.state     
-    return <div style={{height:"100%" }}>
+  render() {
+    let { datas } = this.state
+    return <div style={{ height: "100%" }}>
       <Layout style={{ marginLeft: 200 }}>
-           <div className="demo-infinite-container"  ref="find" >
-              { datas.map((item,index)=>
-                          <div className="demo_box" key={item._id}  >  
-                          <img src={item.img} className="demo_img"   onError={this.errorPic.bind(this, ('tygimg' + index)) }  ref={"tygimg" + index}      />
-                          <h4  className="deom_store">{item.brandName}</h4>
-                          <h4  className="deom_price">{item.price}元/张</h4>
-                          <div className="demo_specification">
-                                <p>{item.goodsName}</p>
-                                <p>{item.specString}</p>
-                          </div>
-                          <div  className="demo_button">
-                          <ButtonGroup>
-                              <Button type="primary">
-                                <Icon type="left" />
-                                  修改
+        <div className="demo-infinite-container" ref="find" >
+          {datas.map((item, index) =>
+            <div className="demo_box" key={item._id}  >
+              <img src={item.img} className="demo_img" onError={this.errorPic.bind(this, ('tygimg' + index))} ref={"tygimg" + index} />
+              <h4 className="deom_store">{item.brandName}</h4>
+              <h4 className="deom_price">{item.price}元/张</h4>
+              <div className="demo_specification">
+                <p>{item.goodsName}</p>
+                <p>{item.specString}</p>
+              </div>
+              <div className="demo_button">
+                <ButtonGroup>
+                  <Button type="primary">
+                    <Icon type="left" />
+                    修改
                               </Button>
-                              <Button type="danger" onClick={this.renmoid.bind(this,item.goodsId)}   >
-                                  删除
+                  <Button type="danger" onClick={this.renmoid.bind(this, item.goodsId)}   >
+                    删除
                                 <Icon type="close-square" />
-                              </Button>
-                          </ButtonGroup>
-                     </div>
-                </div>          
-              )}
-          </div>
-       </Layout>
-   </div>
-    }
+                  </Button>
+                </ButtonGroup>
+              </div>
+            </div>
+          )}
+        </div>
+      </Layout>
+    </div>
+  }
 }
 
 export default Addlist
--- src/static/subassembly/merchandise/commit.js
+++ src/static/subassembly/merchandise/commit.js
@@ -1,157 +1,160 @@
-
-import React, {Component} from "react";
-import {
-    Form,
-    Input,
-    Tooltip,
-    Icon,
-    Cascader,
-    Select,
-    Row,
-    Col,
-    Checkbox,
-    Button,
-    AutoComplete,
-  } from 'antd';
-  const { Option } = Select;
-  const AutoCompleteOption = AutoComplete.Option;
-  class RegistrationForm extends React.Component {
-    state = {
-      confirmDirty: false,
-      autoCompleteResult: [],
-    };
-  
-    handleSubmit = e => {
-      e.preventDefault();
-      this.props.form.validateFieldsAndScroll((err, values) => {
-        if (!err) {
-          console.log('Received values of form: ', values);
-        }
-      });
-    };
-  
-    handleConfirmBlur = e => {
-      const { value } = e.target;
-      this.setState({ confirmDirty: this.state.confirmDirty || !!value });
-    };
-  
-    compareToFirstPassword = (rule, value, callback) => {
-      const { form } = this.props;
-      if (value && value !== form.getFieldValue('password')) {
-        callback('Two passwords that you enter is inconsistent!');
-      } else {
-        callback();
-      }
-    };
-  
-    validateToNextPassword = (rule, value, callback) => {
-      const { form } = this.props;
-      if (value && this.state.confirmDirty) {
-        form.validateFields(['confirm'], { force: true });
-      }
-      callback();
-    };
-  
-    handleWebsiteChange = value => {
-      let autoCompleteResult;
-      if (!value) {
-        autoCompleteResult = [];
-      } else {
-        autoCompleteResult = ['.com', '.org', '.net'].map(domain => `${value}${domain}`);
-      }
-      this.setState({ autoCompleteResult });
-    };
-  
-    render() {
-      const { getFieldDecorator } = this.props.form;
-      const { autoCompleteResult } = this.state;
-  
-      const formItemLayout = {
-        labelCol: {
-          xs: { span: 24 },
-          sm: { span: 8 },
-        },
-        wrapperCol: {
-          xs: { span: 24 },
-          sm: { span: 16 },
-        },
-      };
-      const tailFormItemLayout = {
-        wrapperCol: {
-          xs: {
-            span: 24,
-            offset: 0,
-          },
-          sm: {
-            span: 16,
-            offset: 8,
-          },
-        },
-      };
-      const prefixSelector = getFieldDecorator('prefix', {
-        initialValue: '86',
-      })(
-        <Select style={{ width: 70 }}>
-          <Option value="86">+86</Option>
-          <Option value="87">+87</Option>
-        </Select>,
-      );
-  
-      const websiteOptions = autoCompleteResult.map(website => (
-        <AutoCompleteOption key={website}>{website}</AutoCompleteOption>
-      ));
-  
-      return (
-        <Form {...formItemLayout} onSubmit={this.handleSubmit}>
-          <Form.Item
-            label={
-              <span>
-                合作商家
-                <Tooltip title="What do you want others to call you?">
-                  <Icon  />
-                </Tooltip>
-              </span>
-            }
-          >
-            {getFieldDecorator('nickname', {
-              rules: [{ required: true, message: '数据不能为空!', whitespace: true }],
-            })(<Input />)}
-          </Form.Item>
-
-
-          <Form.Item
-            label={
-              <span>
-                规格
-                <Tooltip title="What do you want others to call you?">
-                  <Icon  />
-                </Tooltip>
-              </span>
-            }
-          >
-            {getFieldDecorator('nickname', {
-              rules: [{ required: true, message: '数据不能为空!', whitespace: true }],
-            })(<Input />)}
-          </Form.Item>
-          
-          <Form.Item {...tailFormItemLayout}>
-            {getFieldDecorator('agreement', {
-              valuePropName: 'checked',
-              initialValue: true,
-            })(
-              <Checkbox>
-               添加一组数据 <a href=""></a>
-              </Checkbox>,
-            )}
-          </Form.Item>
-         <Form.Item {...tailFormItemLayout}>
-            <Button type="primary" htmlType="submit">
-              添加
-            </Button>
-          </Form.Item>
-        </Form> 
+import React, { Component } from "react";
+import { Upload, Form, Input, Select, Icon, Button } from "antd";
+import "./commit.scss";
+import axios from "axios";
+function getBase64(img, callback) {
+  const reader = new FileReader();
+  reader.addEventListener('load', () => callback(reader.result));
+  reader.readAsDataURL(img);
+}
+class Goodlist extends Component {
+  state = {
+    imageUrl: '',
+    loading: false,
+    imgurl: ''
+  }
+  handleChange = info => {
+    console.log(info);
+    if (info.file.status === 'uploading') {
+      this.setState({ loading: true });
+      return;
+    }
+    if (info.file.status === 'done') {
+      let imgurl = info.file.response.data.filename;
+      // console.log(imgurl);
+      getBase64(info.file.originFileObj, imageUrl =>
+        this.setState({
+          imageUrl,
+          loading: false,
+          imgurl
+        }),
       );
     }
   }
-  
-  export default Form.create()(RegistrationForm);
+  commitData = () => {
+    console.log(this.props.form.getFieldsValue());
+    let uploadData = this.props.form.getFieldsValue();
+    if (uploadData.brandname && uploadData.goodsname && uploadData.price && uploadData.selectMultiple) {
+      // console.log(6666);
+      let packages = uploadData.selectMultiple.map((item, idx) => ({
+        id: idx * 1 + 1,
+        name: item
+      }));
+      let query = {
+        brandId: Date.now(),
+        brandName: uploadData.brandname,
+        cateId: 335,
+        cateName: '地砖',
+        floatPrice: '0.00',
+        floatPriceUnit: '元/片',
+        goodsId: Date.now() + 1,
+        goodsName: uploadData.goodsname,
+        img: `http://47.98.245.185:12345/static/${this.state.imgurl}`,
+        jingle: '',
+        marketPrice: '348.000',
+        packages,
+        price: uploadData.price,
+        priceType: 2,
+        saleNum: 440,
+        skuId: Date.now() + 2,
+        spec: '',
+        specString: "类别: 抛釉砖;地砖尺寸: 600*1200",
+        scheduling: 1,
+        unit: '片',
+        timing: 0,
+        afterTimingState: null
+      }
+      let { data } = axios.post('http://localhost:1998/goodinf', {
+        query
+      });
+      // console.log(data);
+      this.props.history.push('/addlist');
+    }
+  }
+  render() {
+    const { getFieldDecorator } = this.props.form;
+    const { imageUrl } = this.state;
+    // console.log(imageUrl);
+    const uploadButton = (
+      <div>
+        <Icon type={this.state.loading ? 'loading' : 'plus'} />
+        <div className="ant-upload-text">上传图片</div>
+      </div>
+    );
+    return <div className="goodlist">
+      <Form.Item
+        labelCol={{ span: 3 }}
+        wrapperCol={{ span: 18 }}
+        label="商品名称"
+      >
+        {getFieldDecorator('goodsname', {
+          rules: [
+            {
+              required: true,
+              message: '请输入商品名',
+            },
+          ],
+        })(<Input placeholder="请输入商品名" />)}
+      </Form.Item>
+      <Form.Item
+        labelCol={{ span: 3 }}
+        wrapperCol={{ span: 18 }}
+        label="商品品牌"
+      >
+        {getFieldDecorator('brandname', {
+          rules: [
+            {
+              required: true,
+              message: '请输入品牌名',
+            },
+          ],
+        })(<Input placeholder="请输入品牌名" />)}
+      </Form.Item>
+      <Form.Item
+        labelCol={{ span: 3 }}
+        wrapperCol={{ span: 18 }}
+        label="商品价格(元)"
+      >
+        {getFieldDecorator('price', {
+          rules: [
+            {
+              required: true,
+              pattern: new RegExp(/^[1-9]\d*$/, "ig"),
+              message: '请输入数字',
+            }
+          ],
+        })(<Input placeholder="请输入价格" />)}
+      </Form.Item>
+      <Form.Item
+        label="标签[多选]"
+        labelCol={{ span: 3 }}
+        wrapperCol={{ span: 18 }}
+      >
+        {getFieldDecorator('selectMultiple', {
+          rules: [
+            { required: true, message: '请选择你的标签', type: 'array' },
+          ],
+        })(
+          <Select mode="multiple" style={{ width: '100%' }} placeholder="请选择商品标签">
+            <Select.Option value="主材包">主材包</Select.Option>
+            <Select.Option value="整装包">整装包</Select.Option>
+            <Select.Option value="拎包入住">拎包入住</Select.Option>
+          </Select>,
+        )}
+      </Form.Item>
+      <Upload
+        name="gd"
+        listType="picture-card"
+        className="avatar-uploader"
+        showUploadList={false}
+        action="http://localhost:1998/goodinf/pic"
+        onChange={this.handleChange}
+      >
+        {imageUrl ? <img src={imageUrl} alt="avatar" style={{ width: '100%' }} /> : uploadButton}
+      </Upload>
+      <Button size="large" type="primary" onClick={this.commitData}>上传</Button>
+    </div >
+  }
+}
+
+export default Form.create()(Goodlist);
